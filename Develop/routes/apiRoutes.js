f0fef5350932617615db41560c4758e62a7c74bc
Marina <mgdelneri@gmail.com>
2021-04-23 14:28:20 -0500

    Started coding apiRoutes.js file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,32 +1,25 @@
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// Linking routes to data source, aka db.json file.
 
-/*const tableData = require('../data/tableData');
-const waitListData = require('../data/waitinglistData');
+const notesData = require('../db/db.json');
 
 // ROUTING
 
 module.exports = (app) => {
-  // API GET Requests
+  // API GET Request
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
-  app.get('/api/tables', (req, res) => res.json(tableData));
-
-  app.get('/api/waitlist', (req, res) => res.json(waitListData));
+  app.get('/api/notes', (req, res) => res.json(notesData));
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // (ex. User writes out a note... this data is then sent to the server...
+  // Then the server saves the data to the notesData json)
   // ---------------------------------------------------------------------------
 
-  app.post('/api/tables', (req, res) => {
+  /*app.post('/api/notes', (req, res) => {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
@@ -48,5 +41,5 @@ module.exports = (app) => {
     waitListData.length = 0;
 
     res.json({ ok: true });
-  });
-};*/
+  });*/
+};
