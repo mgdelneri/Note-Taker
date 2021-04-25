74e94c640b0b057d6e1b13c504ccc96bc36df4b6
Marina <mgdelneri@gmail.com>
2021-04-25 13:34:24 -0500

    Set up server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,23 @@
+const fs = require('fs');
+// const express = require('express');
+
+// const app = express();
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
 // LOAD DATA
 // Linking routes to data source, aka db.json file.
 
-const notesData = require('../db/db.json');
+// const notesData = require('../db/db.json');
 
 // ROUTING
 
 module.exports = (app) => {
   // API GET Request
 
-  app.get('/api/notes', (req, res) => res.json(notesData));
+  app.get('/api/notes', (req, res) => {
+      fs.ReadFile()};
   
   // API POST Requests
   app.post('/api/notes', (req, res) => {
