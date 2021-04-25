68a6d5cdaa3f9081bd9995767ad7399d5856e5c2
Marina <mgdelneri@gmail.com>
2021-04-25 15:39:08 -0500

    Added code in apiRoutes to parse the data requested

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,20 @@
 const fs = require('fs');
-// const express = require('express');
-
-// const app = express();
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// LOAD DATA
-// Linking routes to data source, aka db.json file.
-
-// const notesData = require('../db/db.json');
 
 // ROUTING
-
 module.exports = (app) => {
   // API GET Request
 
   app.get('/api/notes', (req, res) => {
-      fs.ReadFile()};
+    fs.readFile(__dirname + '/../db/db.json', 'utf8', (error, data) => {
+    error ? console.error(error) : console.log(data);
+    let dataJson = JSON.parse(data);
+    res.json(dataJson);
+    })
+  })
   
   // API POST Requests
   app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-
-    console.log(newNote);
-
-    notesData.push(newNote);
-
-    res.json(newNote);
+    res.send("POST Request Called")
+    //?????
   })
 }
